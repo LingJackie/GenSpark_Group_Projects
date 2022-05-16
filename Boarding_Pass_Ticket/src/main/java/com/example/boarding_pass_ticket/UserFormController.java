cbe7c5d79784673b3ab1633a4341a7819cf78819
Jackie Ling <jackielinggggg@gmail.com>
2022-05-16 10:24:22 -0400

    Renamed files

--- Boarding_Pass_Ticket/src/main/java/com/example/boarding_pass_ticket/UserFormController.java
+++ Boarding_Pass_Ticket/src/main/java/com/example/boarding_pass_ticket/UserFormController.java
@@ -3,7 +3,7 @@ package com.example.boarding_pass_ticket;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class UserFormController {
     @FXML
     private Label welcomeText;
 
