35176376686cab06c1d18f796facbaa201d8923b
EliasLavandera <102991754+EliasLavandera@users.noreply.github.com>
2022-05-16 08:24:15 -0700

    BoardingPass

---
+++ BordingPassNumber.java
@@ -0,0 +1,8 @@
+import java.util.UUID;
+
+public class BordingPassNumber {
+    public String boardingPassNumber(String uniqueNumber){
+        UUID uniqueBoardingNumber = UUID.randomUUID();
+        return uniqueNumber;
+    }
+}
