9e15395ccb1b303c8e0a9546980a06ad90426fcc
Jackie Ling <jackielinggggg@gmail.com>
2022-05-16 11:15:17 -0400

    Added a Boarding Pass Class

---
+++ Boarding_Pass_Ticket/src/main/java/com/example/boarding_pass_ticket/BoardingPass.java
@@ -0,0 +1,36 @@
+package com.example.boarding_pass_ticket;
+
+import java.text.DateFormat;
+import java.time.LocalTime;
+
+public class BoardingPass {
+    String boardingPassNum;
+
+//    DateFormat date;
+    String date; // DATATYPE SUBJECT TO CHANGE
+
+    String origin;
+    String Destination;
+    String eta; // DATATYPE SUBJECT TO CHANGE
+
+//    LocalTime departureTime;
+    String departureTime; // DATATYPE SUBJECT TO CHANGE
+
+    String name;
+    String email;
+    int phoneNum;
+    String gender;
+    int age;
+
+    int totalTicketPrice;
+
+    public BoardingPass(String name, String email, int phoneNum, String gender, int age, String date, String departureTime){
+        this.name=name;
+        this.email=email;
+        this.phoneNum=phoneNum;
+        this.gender=gender;
+        this.age=age;
+        this.date=date;
+        this.departureTime=departureTime;
+    }
+}
--- Boarding_Pass_Ticket/src/main/java/com/example/boarding_pass_ticket/UserFormController.java
+++ Boarding_Pass_Ticket/src/main/java/com/example/boarding_pass_ticket/UserFormController.java
@@ -1,14 +1,29 @@
 package com.example.boarding_pass_ticket;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class UserFormController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private Text actiontarget;
+
+
+    @FXML
+    private TextField bagel;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void handleSubmitButtonAction(ActionEvent actionEvent) {
+        String bagelText = bagel.getText();
+        actiontarget.setText("Sign in button pressed"+ bagelText);
     }
 }
